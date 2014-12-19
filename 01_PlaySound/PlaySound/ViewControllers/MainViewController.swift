c8a5f2ea576734c9549832ef65541ebed990aabf
Typoon <apisdk@gmail.com>
2014-12-19 18:47:17 +0900

    implements for viewDidLoad(init variable)

--- 01_PlaySound/PlaySound/ViewControllers/MainViewController.swift
+++ 01_PlaySound/PlaySound/ViewControllers/MainViewController.swift
@@ -11,9 +11,24 @@ import AVFoundation
 
 class MainViewController: UIViewController, AVAudioPlayerDelegate
 {
+    var audioPlayer: AVAudioPlayer?
+    var progressTimer: NSTimer?
+    
     @IBOutlet var progressSlider:UISlider!
     @IBOutlet var volumeSlider:UISlider!
     @IBOutlet var volumeLabel:UILabel!
     
+    override func viewDidLoad() {
+        // get URL for mp3 file
+        var bundleURL: NSURL = NSBundle.mainBundle().URLForResource("sound", withExtension: "mp3")!
+        
+        // init player
+        self.audioPlayer = AVAudioPlayer(contentsOfURL: bundleURL, error: nil)
+        
+        self.audioPlayer!.delegate = self
+        self.progressSlider!.value = 0
+        
+        super.viewDidLoad()
+    }
 }
 
