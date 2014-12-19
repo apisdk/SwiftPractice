cef86ac182d3040978ec4bf55b521f4d775f1167
Typoon <apisdk@gmail.com>
2014-12-19 17:04:07 +0900

    create new project of PlaySound

---
+++ 01_PlaySound/PlaySound/PlaySound/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  PlaySound
+//
+//  Created by 윤태훈 on 2014. 12. 19..
+//  Copyright (c) 2014년 apisdk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ 01_PlaySound/PlaySound/PlaySound/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlaySound
+//
+//  Created by 윤태훈 on 2014. 12. 19..
+//  Copyright (c) 2014년 apisdk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
