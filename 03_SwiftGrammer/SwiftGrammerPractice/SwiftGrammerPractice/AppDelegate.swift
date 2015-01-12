ce54689f9d0ecefe1d3c51680e0954b34d077032
Typoon <apisdk@gmail.com>
2015-01-12 12:15:15 +0900

    Add proejct for swift practice

---
+++ 03_SwiftGrammer/SwiftGrammerPractice/SwiftGrammerPractice/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftGrammerPractice
+//
+//  Created by Typoon on 2015. 1. 12..
+//  Copyright (c) 2015년 apisdk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 03_SwiftGrammer/SwiftGrammerPractice/SwiftGrammerPractice/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftGrammerPractice
+//
+//  Created by Typoon on 2015. 1. 12..
+//  Copyright (c) 2015년 apisdk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
