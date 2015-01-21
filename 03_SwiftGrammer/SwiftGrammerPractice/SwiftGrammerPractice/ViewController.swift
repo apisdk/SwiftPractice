406f3775cb1cf5706a05072eb8f05d0399201cc3
Typoon <apisdk@gmail.com>
2015-01-22 00:00:54 +0900

    add example of drawing NSView

--- 03_SwiftGrammer/SwiftGrammerPractice/SwiftGrammerPractice/ViewController.swift
+++ 03_SwiftGrammer/SwiftGrammerPractice/SwiftGrammerPractice/ViewController.swift
@@ -9,15 +9,64 @@
 import Cocoa
 
 
+class SquareView: NSView {
+    override func drawRect(dirtyRect: NSRect) {
+        NSColor.greenColor().setFill()
+        NSRectFill(dirtyRect)
+        
+        super.drawRect(dirtyRect)
+    }
+}
 
 class ViewController: NSViewController {
 
+    var user: Person?
+    var userCar: Car?
+    
+    var seller: Person?
+    var sellerCar: Car?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+ 
+        makeSquareView()
+        
+        testMemoryLeak()
     }
 
+    func makeSquareView() {
+        let viewFrame: CGRect = self.view.frame
+        var width: Float = 100
+        var height: Float = 50
+        var positionX: Float = Float(viewFrame.size.width) / 2 - width
+        var positionY: Float = Float(viewFrame.size.height) / 2 - height
+        
+        var mainView: SquareView = SquareView(frame: CGRectMake(CGFloat(width), CGFloat(height), CGFloat(positionX), CGFloat(positionY)))
+        self.view.addSubview(mainView)
+    }
+    
+    func testMemoryLeak() {
+        user = Person(name: "User")
+        userCar = Car(model: "userCar")
+        
+        seller = Person(name: "Seller")
+        sellerCar = Car(model: "sellerCar")
+        
+        user?.car = userCar
+        userCar?.leesee = user
+        
+        seller?.car = sellerCar
+        sellerCar?.seller = seller
+        
+        userCar = nil
+        user = nil
+        
+        seller = nil
+        sellerCar = nil
+    }
+    
     override var representedObject: AnyObject? {
         didSet {
         // Update the view, if already loaded.
